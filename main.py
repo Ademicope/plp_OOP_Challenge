4196fc4bc2f12d31d525a99876eab6466fad9692
Ademicope <michaeladekunle00@gmail.com>
2025-04-17 22:12:46 +0100

    OOP challenge updated

---
+++ main.py
@@ -0,0 +1,9 @@
+from pet import Pet
+
+new_pet = Pet(input("Enter your pet name: "))
+new_pet.eat()
+new_pet.train(input("Enter a trick: "))
+new_pet.show_tricks()
+new_pet.play()
+new_pet.sleep()
+new_pet.get_status()
---
+++ pet.py
@@ -0,0 +1,70 @@
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 5
+        self.energy = 5
+        self.happiness = 5
+        self.tricks = []
+
+    def eat(self):
+        """This function reduces hunger level by 3 but not below 0
+        and increases happiness by 1
+        """
+        if self.hunger <= 2:
+            self.hunger = 0
+        else:
+            self.hunger -= 3
+            self.happiness += 1
+        print(f"Pet ate, happiness increased\n")
+        print(f"Hunger level: {self.hunger}, Happiness_level: {self.happiness}")
+
+    def sleep(self):
+        """This function increases energy level by 5
+          but not above 10
+        """
+        print(f"Energy level: {self.energy}")
+        if self.energy >= 5:
+            self.energy = 10
+        else:
+            self.energy += 5
+        print(f"{self.name} slept.\n")
+        print(f"Current energy level: {self.energy}")
+
+    def play(self):
+        """This function reduces energy level by 2 but not below 0,
+        increases happiness by 2, and increases hunger by 1
+        """
+        if self.energy <= 2:
+            self.energy = 0
+        else:
+            self.energy -= 2
+            self.happiness +=2
+            self.hunger += 1
+        print(f"Pet played, happiness increased\n")
+
+    def train(self, trick):
+        """This function adds a trick to the pet's tricks list,
+        """
+        # trick = self.tricks.append(input("Enter new trick for pet"))
+        self.tricks.append(trick)
+    
+    def show_tricks(self):
+        """This function shows the tricks learned by pet"""
+        
+        # for i in range(len(self.tricks)):
+        #     print(f"Trick {i+1}: {self.tricks[i]}")
+        # print("Tricks learned by the pet:")
+
+        for i, trick in enumerate(self.tricks):
+            print(f"Trick {i+1}: {trick.strip()}")  # Strip removes any extra whitespace
+            print("Tricks learned by the pet:")
+
+    def get_status(self):
+        """This function prints the current state of the pet"""
+        print("Current status of the pet:")
+        print(f"Pet name: {self.name}")
+        print(f"Hunger: {self.hunger}")
+        print(f"Energy: {self.energy}")
+        print(f"Happiness: {self.happiness}")
+        print(f"Tricks learned: {self.tricks}")
+
